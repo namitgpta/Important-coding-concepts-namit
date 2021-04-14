32bc0eb8232b3c6b6545987db62fea22d3c474ef
namitgpta <namitg677@gmail.com>
2021-04-14 19:13:23 +0530

    14 April 2021

---
+++ Java_Programs/Execute_Comments_in_Java_u000d.java
@@ -0,0 +1,24 @@
+package Java_Programs;
+
+public class Execute_Comments_in_Java_u000d {
+
+    public static void main(String[] args) {
+
+        // namit ji(' \u000d System.out.println("inside double slashed comment");
+
+        /*
+        namit ji(' \u000d System.out.println("inside * comment");
+         */
+
+        System.out.println("namit");
+        int i = 10;
+        // \u000di += 10;
+        System.out.println(i);
+
+        /*
+        /u000d is a new line unicode character. It shift to new line.
+         */
+
+    }
+
+}
